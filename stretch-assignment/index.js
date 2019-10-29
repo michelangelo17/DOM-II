88cccac99d021c63686837a93ae61f73ae8088fb
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-29 16:05:11 -0700

    finished 3 of 4 of the stretch on stretch goals

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,34 +1,35 @@
 const blocks = document.querySelectorAll('.block');
-
+const width = window.innerWidth;
+console.log(width);
 blocks.forEach((block, i) => {
+  block.style.order = `${++i}`
+  block.style.transition = 'order .5s linear'
   block.addEventListener('click', () => {
   block.style.order = 0;
   blocks.forEach(block => block.style.order++);
 })});
 
-// blocks.forEach(block => {
-//   block.addEventListener('mousedown', (e) => {
-//     block.style.position = 'relative';
-//     block.style.right = 0;
-//     while (e) {
-//       block.style.right++;
-//     }
-//   })
-// })
-
 let buttonDownTimer;
-
+let buttonUpTimer;
 blocks.forEach(block => {
   let move = 0;
   block.style.position = 'relative';
   block.addEventListener('mousedown', () => {
+    if (buttonUpTimer) clearInterval(buttonUpTimer);
     buttonDownTimer = setInterval(() => {
-      block.style.left = `${move += 10}px`;
+      if (move < (width - 150)) {
+        block.style.left = `${move += 10}px`; 
+      }
     }, 100);
   })
+  document.addEventListener("mouseup", function(){
+    if (buttonDownTimer) clearInterval(buttonDownTimer);
+    buttonUpTimer = setInterval(() => {
+      if (move > 0) {
+        block.style.left = `${move -= 10}px`; 
+      }
+    }, 100);
+});
 });
 
-document.addEventListener("mouseup", function(){
-    if (buttonDownTimer) clearInterval(buttonDownTimer);
-});
 
