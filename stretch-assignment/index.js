6af83c434738280533e16b7984baed0d852b5177
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-29 18:29:09 -0700

    technically finished 4 of 4, but contains minor bugs, will refactor completely

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,35 +1,49 @@
 const blocks = document.querySelectorAll('.block');
 const width = window.innerWidth;
-console.log(width);
+let buttonDownTimer;
+let buttonUpTimer;
+let moveBack = 0;
+let target;
+
 blocks.forEach((block, i) => {
   block.style.order = `${++i}`
   block.style.transition = 'order .5s linear'
   block.addEventListener('click', () => {
-  block.style.order = 0;
-  blocks.forEach(block => block.style.order++);
-})});
-
-let buttonDownTimer;
-let buttonUpTimer;
-blocks.forEach(block => {
-  let move = 0;
-  block.style.position = 'relative';
-  block.addEventListener('mousedown', () => {
-    if (buttonUpTimer) clearInterval(buttonUpTimer);
-    buttonDownTimer = setInterval(() => {
-      if (move < (width - 150)) {
-        block.style.left = `${move += 10}px`; 
-      }
-    }, 100);
+    block.style.order = 0;
+    blocks.forEach(block => block.style.order++);
   })
-  document.addEventListener("mouseup", function(){
-    if (buttonDownTimer) clearInterval(buttonDownTimer);
+});
+
+blocks.forEach(block => {
+  block.addEventListener('mousedown', moveRight)
+});
+
+function moveRight() {
+  target = this;
+  move = 0;
+  if (moveBack <= 0) {
+    move = 0;
+  } else {
+    move = moveBack;
+  }
+
+  this.style.position = 'relative';
+  buttonDownTimer = setInterval(() => {
+    if (buttonUpTimer) clearInterval(buttonUpTimer);
+    if (move < (width - 150)) {
+      this.style.left = `${move += 10}px`;
+      moveBack = move;
+    }
+  }, 100);
+}
+
+document.addEventListener('mouseup', () => {
+  if (buttonDownTimer) {
+    clearInterval(buttonDownTimer);
     buttonUpTimer = setInterval(() => {
-      if (move > 0) {
-        block.style.left = `${move -= 10}px`; 
+      if (moveBack > 0) {
+        target.style.left = `${moveBack -= 10}px`;
       }
     }, 100);
-});
-});
-
-
+  }
+})
